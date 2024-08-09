c335db1fd91e24bc85ebb1527813257d6e9b73a5
Sergey <segadev85@gmail.com>
2024-08-10 00:42:41 +0300

    HW4

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HW2 } from "./HW/2/HW2";
 import { HW1 } from "./HW/1/HW1";
 import React from "react";
 import { HW3 } from "./HW/3/HW3";
+import { HW4 } from "./HW/4/HW4";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <HW1 />
       <HW2 />
       <HW3 />
-      {/*<HW4 />*/}
+      <HW4 />
     </div>
   );
 }
